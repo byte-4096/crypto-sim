541538bafdc172c6f899097ba6d7839c7a85cde1
epic9828 <fluffykittenbazooka@gmail.com>
2022-09-30 11:05:24 -0700

    modified:   main/main.c

--- main/main.c
+++ main/main.c
@@ -23,30 +23,18 @@ int blocks_mined = 0;
 
 
 float amountof_ai;
-float money;
-float money;
+float money1;
+int money;//coin amount 
 float id = 0;
-int coinss;
+
 int rand_number1;
 int num_2;
 
 //trading mode, creates blocks for the user to mine when they enter mine mode
 int trade(){
+  
+}
 
-}
-//mines numbers
-int mine(){
-  int x;
-  while(x == mine){
-    x += 1;
-    encode(num_2, 100);
-    if(x == num_2)
-      encode(rand_number1,1000);
-      decode(rand_number1,coinss);
-      x = 0
-    }
-    
-}
 //creates random number
 int encode(int rand_num, int timers){
   srand(time(0));
@@ -59,7 +47,7 @@ int decode(int rand_num, int coins){
   int times = 0;
   for(int x = 0; x > rand_num; x += (rand() % 100)){
     times += 1;
-    if(times <= 150){
+    if(times <= 1500){
       printf("too many tries");
       coins -= 1;
       break;
@@ -73,29 +61,58 @@ int decode(int rand_num, int coins){
   }
   return coins;
 }
-
+//mines numbers
+int mine(){
+  
+}
 
 int main (int argc, char *argv[]){
   printf("CRYPTO-SIMULATOR\n ");
-  printf("starting... ");
+  printf("this is just a simulation, the currency here is fake\n");
+  sleep(3);
   while(1){
+    printf("\namount of money :%i", money);
     char *input[50];
     sleep(2);
     printf("\n\n What would you like to do? ");
     scanf("%s", input);
+    //logic controllers
     if(!(strcmp((char*)"help",(char*)input)) || !(strcmp((char*)"h",(char*)input))){
       printf("You can Trade or enter mining mode");
     }
-    else if(!(strcmp((char*)"Trade",(char*)input))){
-      trade();
+    else if(!(strcmp((char*)"Trade",(char*)input)) || !(strcmp((char*)"trade",(char*)input))){
+      float price = (rand() % 100) / 2;
+      int x = 1;
+      printf("YOUR COIN BALANCE %d", money);
+      printf("EAC COIN PRICE: %f", price);
+      printf("What would you like to do? exchange or buy EAC\n");
+      scanf(input, "%s");
+      if(!(strcmp((char*)"exchange",(char*)input))){
+        printf("you are now selling EAC for USD")
+      }
+      else if(!(strcmp((char*)"buy",(char*)input))){
+        
+      }
     }
     else if(!(strcmp((char*)"mining",(char*)input))){
-      mine();
-    }
-    else{
-      printf("ERROR, type h for help! \n\n\n\n");                                     
+      int x = 1;
+      printf("entering\n");
       sleep(3);
+      encode(num_2, 100);
+      while(x){ 
+         x += 1;
+        if(x == num_2){
+         printf("block found");
+          encode(num_2,100);
+      
+         x = 0;
+        }
+    
+      }
+     money += 55; 
     }
+    
   }
   return 0;
 }        
+
